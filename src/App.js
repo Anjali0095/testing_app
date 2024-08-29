50d9ee8bab756564b7b7a273aef1e6be7de817ee
Anjali <95272898+Anjali0095@users.noreply.github.com>
2024-08-29 12:19:20 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>ghyiuyew🔥🔥</h1>
         <h1>Learn React with us!😍😍🔥</h1>
       </header>
     </div>
